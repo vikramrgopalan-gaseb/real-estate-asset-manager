d3e30d466e0f727641d80309b2db2570f00562c4
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-05-10 15:29:35 -0600

    5/10 v4

--- realestateassetmanager/my_app/views.py
+++ realestateassetmanager/my_app/views.py
@@ -10,18 +10,17 @@ from .models import Building, Floor
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how we create a 'user' form object
-    # that includes the data from the browser
+    
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
+     
       user = form.save()
-      # This is how we log a user in via code
+      
       login(request, user)
       return redirect('building-list')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
+  
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
