f990f47816d96460c587a9428f29d1378f60d9ce
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-05-10 11:21:57 -0600

    5/10 v2

--- realestateassetmanager/my_app/urls.py
+++ realestateassetmanager/my_app/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.BuildingList.as_view(), name='building-list'),
-    path('building/<int:pk>/', views.BuildingDetail.as_view(), name='building-detail'), # MUST be 'building-detail'
-    path('building/<int:building_pk>/floor/add/', views.FloorCreate.as_view(), name='floor-create'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('building/<int:pk>/', views.BuildingDetail.as_view(), name='building-detail'),
     path('building/add/', views.BuildingCreate.as_view(), name='building-create'),
-    path('floor/add/', views.FloorCreate.as_view(), name='floor-create'),
-    path('floor/<int:pk>/update/', views.FloorUpdate.as_view(), name='floor-update'),
+    path('building/<int:building_pk>/floor/add/', views.FloorCreate.as_view(), name='floor-create'),
+    path('floor/<int:pk>/edit/', views.FloorUpdate.as_view(), name='floor-update'),
     path('floor/<int:pk>/delete/', views.FloorDelete.as_view(), name='floor-delete'),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- realestateassetmanager/my_app/views.py
+++ realestateassetmanager/my_app/views.py
@@ -1,83 +1,55 @@
-from django.views import generic
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from .models import Building, Floor
 
-# BUILDING VIEWS
+# AUTHENTICATION
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('building-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+# BUILDING CRUD
 class BuildingList(LoginRequiredMixin, ListView):
     model = Building
-
     def get_queryset(self):
-        # Only return buildings owned by the current user
         return Building.objects.filter(owner=self.request.user)
 
 class BuildingDetail(LoginRequiredMixin, DetailView):
     model = Building
 
-    def get_queryset(self):
-        # Ensures that if a user tries to access a building ID they don't own, 
-        # Django will throw a 404 "Not Found" error
-        return Building.objects.filter(owner=self.request.user)
-
 class BuildingCreate(LoginRequiredMixin, CreateView):
     model = Building
-    fields = ['name', 'address']
+    fields = ['name', 'address', 'svg_filename']
     success_url = reverse_lazy('building-list')
-
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
-# FLOOR VIEWS
+# FLOOR CRUD
 class FloorCreate(LoginRequiredMixin, CreateView):
     model = Floor
     fields = ['floor_number', 'floor_type', 'annual_income']
-    success_url = reverse_lazy('building-list')
-
     def form_valid(self, form):
-        # Capture the building ID from the URL (e.g., /building/5/floor/add/)
-        building_id = self.kwargs.get('building_pk')
-        form.instance.building = Building.objects.get(pk=building_id)
+        form.instance.building = Building.objects.get(pk=self.kwargs['building_pk'])
         return super().form_valid(form)
-
     def get_success_url(self):
-        # Redirect back to the specific building we just added a floor to
         return reverse_lazy('building-detail', kwargs={'pk': self.kwargs['building_pk']})
 
 class FloorUpdate(LoginRequiredMixin, UpdateView):
     model = Floor
     fields = ['floor_type', 'annual_income']
-    template_name = 'my_app/floor_form.html'
-
     def get_success_url(self):
-        # self.object refers to the specific Floor being updated
-        # We grab its parent building's ID to build the redirect URL
         return reverse_lazy('building-detail', kwargs={'pk': self.object.building.id})
 
 class FloorDelete(LoginRequiredMixin, DeleteView):
     model = Floor
-
     def get_success_url(self):
-        # Redirect back to the building detail page
         return reverse_lazy('building-detail', kwargs={'pk': self.object.building.id})
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('building-list')
-    template_name = 'registration/signup.html'
-
-    def form_valid(self, form):
-        # 1. Save the user to the database
-        user = form.save()
-        # 2. Log the user in manually
-        login(self.request, user)
-        # 3. Redirect to the success_url (portfolio)
-        return super().form_valid(form)
